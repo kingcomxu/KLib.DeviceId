62eabadccfd24a15d4d7b93b35cd64604586c138
kingcomxu <kingcomxu@vip.qq.com>
2024-02-06 16:11:35 +0800

    init

---
+++ KLib.DeviceId.Demo/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace KLib.DeviceId.Demo;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        KxDeviceIdBuilder builder = new KxDeviceIdBuilder();
+
+        builder
+            .AddMachineName()              //from Environment
+            .AddOSVersion()                //from Environment 
+            .AddMacAddress()                //NetworkInterface 
+            .AddMachineGuid()               //from registry LocalMachine SOFTWARE\Microsoft\Cryptography MachineGuid
+            .AddWindowsProductId()          //from registry LocalMachine SOFTWARE\Microsoft\Windows NT\CurrentVersion ProductId
+            .AddWindowsDeviceId()           //from registry LocalMachine SOFTWARE\Microsoft\SQMClient MachineId
+            .AddWindowsRegistryToken()      //from registry CurrentUser SOFTWARE\KLib.DeviceIdToken valueName ?? Process.GetCurrentProcess().ProcessName 
+            .AddProcessorInfo()             //WmiLight  SELECT * FROM Win32_Processor
+            .AddSystemUuid()                //WmiLight  UUID FROM Win32_ComputerSystemProduct
+            .AddMotherboardInfo()           //WmiLight  SELECT * FROM Win32_BaseBoard 
+            .AddSystemDriveInfo()           //WmiLight  Win32_DiskPartition Win32_DiskDrive 
+            ;
+
+         
+        Console.WriteLine(builder.ToString()); 
+        Console.WriteLine(builder.ToHashString()); 
+         
+        Console.ReadKey();
+    }
+}
